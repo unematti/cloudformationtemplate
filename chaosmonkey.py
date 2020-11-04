f9ab9192c0009e6033aa5ed7693dcb314cb8162a
unematti <ujvarimatyas@gmail.com>
2020-11-04 18:20:55 +0100

    remove extra.cf, added chaosmonkey.py to randomly terminate an instance in EC2

---
+++ chaosmonkey.py
@@ -0,0 +1,18 @@
+from random import randint as randint
+import boto3
+
+
+ec2 = boto3.client('ec2')
+response = ec2.describe_instances()
+waiter = ec2.get_waiter('instance_terminated')
+
+lenght = len(response['Reservations'])
+if not lenght: 
+    exit('There are no instances to terminate')
+choose = randint( 0, lenght)
+
+instance = (response['Reservations'][choose]['Instances'][0]['InstanceId'])
+print('Terminating instance:', instance, 'out of', lenght, 'instances' )
+ec2.terminate_instances(InstanceIds = [instance])
+waiter.wait(InstanceIds = [instance])
+print('done')
