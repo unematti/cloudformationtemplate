968dd6d5cd126c9c2da891b76f0c38cb8c949f4d
unematti <ujvarimatyas@gmail.com>
2020-11-04 13:13:09 +0100

    rename InstanceWithSecurity.cf to Autoscale.yaml, modified cleanupaws.py

--- cleanupaws.py
+++ cleanupaws.py
@@ -5,31 +5,34 @@ cf = boto3.client('cloudformation')
 s3 = boto3.resource('s3')
 ec2 = boto3.client('ec2')
 
+instancewaiter = ec2.get_waiter('instance_terminated')
+stackwaiter = cf.get_waiter('stack_delete_complete')
 securitygroups = ec2.describe_security_groups()
 instances = ec2.describe_instances()
 cfresponse = cf.describe_stacks()
 
 
+print('\033[91m' + 'Deleting Stacks' + '\033[0m')
+
 for stack in cfresponse['Stacks']:
-    print('for1', stack['StackStatus'])
+    print('Deleting ' ,stack['StackName'])
     if 'RootId' not in stack and stack['StackStatus'] == 'CREATE_COMPLETE':
-        print('if1',stack['StackName'])
         cf.delete_stack(StackName = stack['StackName'])
-        while True: 
-            try: 
-                thing = cf.describe_stack_events(StackName = stack['StackName'])
-            except:
-                break
-            print('waiting')
-            time.sleep(15)
+        stackwaiter.wait(StackName = stack['StackName'])
         print('deletion of stack', stack['StackName'], 'is complete')
+
+print('\033[91m' + 'Deleting remainder Instances' + '\033[0m')
 
 for reservation in instances["Reservations"]: #checks for remaining instances in ec2
     for instance in reservation["Instances"]:
         if instance['State']['Name'] != 'terminated': 
+            print('Deleting ', instance['InstanceId'])
             ec2.terminate_instances(InstanceIds = [instance['InstanceId']]) #terminates remaining instances
+            instancewaiter.wait(InstanceIds = [instance['InstanceId']])
             print('instance', instance['InstanceId'], 'has been terminated')
             time.sleep(5)
+
+print('\033[91m' + 'Deleting remainder Security Groups' + '\033[0m')
 
 for group in securitygroups['SecurityGroups']: #looks for remaining security groups
     print(group['GroupId'])
@@ -40,5 +43,5 @@ for group in securitygroups['SecurityGroups']: #looks for remaining security gro
         print('the group couldnt be deleted')
 
 
-for bucket in s3.buckets.all():
-    print(bucket.name)
+#for bucket in s3.buckets.all():
+#    print(bucket.name)
